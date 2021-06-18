bc68559f37eb0860506f02854ebf69f000f01544
Shafi KHan <76864645+SaMs-Hub@users.noreply.github.com>
2021-06-18 17:15:55 +0530

    Create Search.py

---
+++ Arrays/Two Dimensional Arrays/Searching a 2D Array/Search.py
@@ -0,0 +1,13 @@
+import numpy as np
+twoDArray = np.array([[11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9] ])
+
+#function to search elements
+def searchTDArray(array, value):
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            if array[i][j] == value:
+                return 'The value is located index '+str(i)+" "+str(j)
+    return 'The element no found'
+
+
+print(searchTDArray(twoDArray, 444))
