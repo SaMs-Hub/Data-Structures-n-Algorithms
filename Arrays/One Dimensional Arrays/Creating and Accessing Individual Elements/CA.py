fa5ea4afd7a6fa1ec1b4b99da9522c9a47cf408d
Shafi KHan <76864645+SaMs-Hub@users.noreply.github.com>
2021-06-18 16:57:21 +0530

    Create CA.py

---
+++ Arrays/One Dimensional Arrays/Creating and Accessing Individual Elements/CA.py
@@ -0,0 +1,11 @@
+from array import *
+
+#creating array
+my_array = array('i',[1,2,3,4,5])
+
+#printing all elements
+for i in my_array:
+    print(i)
+    
+#printing individual element
+print(my_array[3])
