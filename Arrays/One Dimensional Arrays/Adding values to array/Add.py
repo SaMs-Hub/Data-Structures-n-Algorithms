9f920672151849d472b8acf826bab2fc90517cbe
Shafi KHan <76864645+SaMs-Hub@users.noreply.github.com>
2021-06-18 16:59:39 +0530

    Create Add.py

---
+++ Arrays/One Dimensional Arrays/Adding values to array/Add.py
@@ -0,0 +1,12 @@
+from array import *
+
+
+my_array = array('i',[1,2,3,4,5])
+
+#using append, element gets added at the last index
+my_array.append(6)
+print(my_array)
+
+#insert adds the element at selected index position
+my_array.insert(3, 11)
+print(my_array)
