a0ed6c9f5c3b25e2826e82378858fa93129b68ce
sam <syedsamer456@gmail.com>
2021-07-28 08:15:38 +0530

    added logic comments for PrimesCPP

--- Basic Problem Solving/prime2toN.cpp
+++ Basic Problem Solving/prime2toN.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main(){
+    // taking input
     int n, k, d;
     cin >> n;
     k = 2;
+
+    //adding logic
     while(k < n){
         d = 2;
         bool flag = false;
