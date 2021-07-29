c477fdcde8303955c9cfac67db4c3ec381f630d9
sam <syedsamer456@gmail.com>
2021-07-29 08:15:38 +0530

    added logic comments for FahrPy

--- Basic Problem Solving/FahrenheitToCelcius.py
+++ Basic Problem Solving/FahrenheitToCelcius.py
@@ -1,7 +1,10 @@
+#taking input from the user
 fh1 = int(input())
 fh2 = int(input())
 step = int(input())
 cth = fh1
+
+#logic for the problem
 while cth <= fh2:
     c = int((cth - 32) * 5 / 9)
     print(str(cth) + "\t" + str(c))
