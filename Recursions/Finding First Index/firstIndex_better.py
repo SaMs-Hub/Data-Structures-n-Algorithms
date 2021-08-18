dfe5e934baa06888918781d8ad8942a409d54b2c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-08-18 23:19:14 +0530

    Create firstIndex_better.py
    Created si as starting index to avoid memory problem

---
+++ Recursions/Finding First Index/firstIndex_better.py
@@ -0,0 +1,14 @@
+def firstIndexBetter(a, x, si):
+    # base case
+    l = len(a)
+    if l == si:
+        return -1
+    if a[si] == x:
+        return si
+
+    # problem logic
+    listOutput = firstIndexBetter(a, x, si + 1)
+    return listOutput
+
+
+print(firstIndexBetter([1, 2, 34, 5, 22, 2, 2], 2, 0))
