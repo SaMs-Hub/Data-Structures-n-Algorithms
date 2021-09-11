7a35e28d7f29f1b2c53956da248ed84e52319eeb
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-11 15:54:13 +0530

    Create isPalindrome.cpp

---
+++ Basic Problem Solving/isPalindrome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n, rev, l, nClone;
+    
+    rev = 0;
+    cin >> n;
+    nClone = n;
+    while (n > 0){
+        l = n % 10;
+        n = n / 10;
+        rev = rev * 10 + l;
+    }
+    if (nClone == rev){
+        cout << rev << " It is Palindrome" << endl;
+    }else{
+        cout << rev << " Not a Palindrome" << endl;
+    }
+}
