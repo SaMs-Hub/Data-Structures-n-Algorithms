196ca39c2e130e88f7ea076cdf9c7bc93b719ee4
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-11 15:16:53 +0530

    Update SumofEvenNumbers.py

--- Basic Problem Solving/SumofEvenNumbers.py
+++ Basic Problem Solving/SumofEvenNumbers.py
@@ -1,7 +1,18 @@
+# using while loop
 n = int(input())
 count = 2
 sum = 0
 while (count <= n):
   sum += count
   count += 2
+print(sum)
+
+
+#using for loop
+n = int(input())
+sum = 0
+for x in range(0, n + 1):
+  if (x % 2 == 0):
+    sum += x
+
 print(sum)
