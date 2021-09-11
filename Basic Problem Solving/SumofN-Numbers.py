5278200db9463281a43f1dd6e6da8f3860f34b45
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-11 15:16:00 +0530

    Update SumofN-Numbers.py

--- Basic Problem Solving/SumofN-Numbers.py
+++ Basic Problem Solving/SumofN-Numbers.py
@@ -1,8 +1,16 @@
+#using while loop
 n = int(input())
 count = 1
 sum = 0
 while (count <= n):
   sum += count
-  
   count += 1
+print(sum)
+
+#using for loop
+n = int(input())
+sum = 0
+for x in range(0, n + 1):
+  sum += x
+
 print(sum)
