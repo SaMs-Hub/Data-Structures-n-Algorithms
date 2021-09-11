6973b74fca8f77acf33b52b1529e20477c4a2b2d
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-11 15:33:52 +0530

    Create SumofEvenNumbers.cpp

---
+++ Basic Problem Solving/#include<bits/SumofEvenNumbers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, sum, count;
+    cin >> n;
+    sum = 0;
+    count = 1;
+    while (count <= n){
+        if (count % 2 == 0){
+            sum += count;
+        }
+        count += 1;
+
+    }
+
+    cout << sum << endl;
+}
