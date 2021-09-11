b1f34e69b5f97c09edd1a37861920edbccd9b72a
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-11 16:00:47 +0530

    Create SumofEvenOdd.cpp

---
+++ Basic Problem Solving/SumofEvenOdd.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, evenSum, oddSum, d;
+    evenSum = 0;
+    oddSum = 0;
+    cin >> n;
+    while (n > 0){
+        d = n % 10;
+        if (d % 2 == 0){
+            evenSum += d;
+        }else{
+            oddSum += d;
+        }
+        n /= 10;
+    }
+    cout << evenSum <<" " <<  oddSum  << endl;
+}
