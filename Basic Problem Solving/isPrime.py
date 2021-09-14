ea498f3823c7dcfba6738ae287d102fdcc61309e
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-14 22:36:35 +0530

    Create isPrime.py

---
+++ Basic Problem Solving/isPrime.py
@@ -0,0 +1,27 @@
+#using while
+n = int(input())
+
+d = 2
+flag = False
+while (d < n):
+    if (n % d == 0):
+        flag = True
+    d += 1
+if (flag):
+    print("Not Prime")
+else:
+    print("Prime")
+    
+    
+#using for
+n = int(input())
+
+flag = False
+for i in range(2, n - 1):
+    if (n % i == 0):
+        flag = True
+
+if (flag):
+    print("Not Prime")
+else:
+    print("Prime")
