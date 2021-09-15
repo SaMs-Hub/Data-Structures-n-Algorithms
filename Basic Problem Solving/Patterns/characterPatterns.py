ef020033e16731df846eb3ec5ff0c2b001de24c9
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-15 15:36:57 +0530

    Create characterPatterns.py

---
+++ Basic Problem Solving/Patterns/characterPatterns.py
@@ -0,0 +1,41 @@
+ABCD
+ABCD
+ABCD
+ABCD
+
+
+
+n = int(input())
+
+i = 1
+while (i <= n):
+    j = 1
+    while(j <= n):
+        character = chr(ord("A") + j - 1)
+        print(character, end='')
+        j += 1
+    print()
+    i += 1
+    
+    
+
+-----------------
+
+ABCD
+BCDE
+CDEF
+DEFG
+
+
+n = int(input())
+
+i = 1
+while (i <= n):
+    j = 1
+    start = chr(ord("A") + i - 1)
+    while(j <= n):
+        character = chr(ord(start) + j - 1)
+        print(character, end='')
+        j += 1
+    print()
+    i += 1
