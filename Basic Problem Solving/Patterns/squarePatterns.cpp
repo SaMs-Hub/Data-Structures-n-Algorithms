0a45a7bb488276980bbe5a8202c83d65e36c6632
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-15 16:07:46 +0530

    Create squarePatterns.cpp

---
+++ Basic Problem Solving/Patterns/squarePatterns.cpp
@@ -0,0 +1,102 @@
+****
+****
+****
+****
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, i, j;
+    cin >> n;
+    i = 1;
+    while (i <= n){
+        j = 1;
+        while (j <= n){
+            cout << '*';
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+}
+
+
+------------------------
+1111
+2222
+3333
+4444
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, i, j;
+    cin >> n;
+    i = 1;
+    while (i <= n){
+        j = 1;
+        while (j <= n){
+            cout << i;
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+}
+    
+    
+------------------------
+1234
+1234
+1234
+1234
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, i, j;
+    cin >> n;
+    i = 1;
+    while (i <= n){
+        j = 1;
+        while (j <= n){
+            cout << j;
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+}
+    
+    
+    
+------------------------
+4444
+4444
+4444
+4444
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, i, j;
+    cin >> n;
+    i = 1;
+    while (i <= n){
+        j = 1;
+        while (j <= n){
+            cout << n;
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+}
