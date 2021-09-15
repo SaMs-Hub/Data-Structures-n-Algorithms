b678821f7994d5cc407a17d4bf2d80e6ff62c223
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-09-15 15:19:07 +0530

    Update squarePattern.py

--- Basic Problem Solving/Patterns/squarePattern.py
+++ Basic Problem Solving/Patterns/squarePattern.py
@@ -14,3 +14,66 @@ while (i <= n):
         j += 1
     print()
     i += 1
+
+
+------------------------
+1111
+2222
+3333
+4444
+
+
+
+n = int(input())
+
+i = 1
+while (i <= n):
+    j = 1
+    while (j <= n):
+        print(i, end="")
+        j += 1
+    print()
+    i += 1
+    
+    
+------------------------
+1234
+1234
+1234
+1234
+
+
+n = int(input())
+
+i = 1
+while (i <= n):
+    j = 1
+    while (j <= n):
+        print(n - j + 1, end="")
+        j += 1
+    print()
+    i += 1
+    
+    
+    
+------------------------
+4444
+4444
+4444
+4444
+
+n = int(input())
+
+i = 1
+while (i <= n):
+    j = 1
+    while (j <= n):
+        print(n, end="")
+        j += 1
+    print()
+    i += 1
+    
+    
+    
+
+    
