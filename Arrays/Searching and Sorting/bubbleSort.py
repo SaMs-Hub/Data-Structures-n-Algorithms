949397ff80ab89938201cd6cd86a29ad8096c046
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-10-20 00:02:10 +0530

    Create bubbleSort.py

---
+++ Arrays/Searching and Sorting/bubbleSort.py
@@ -0,0 +1,11 @@
+arr = [4, 5, 1, 2, 3]
+
+def func(arr):
+    l = len(arr)
+    for i in range(l - 1):
+        for j in range(l - i - 1):
+            if (arr[j] > arr[j + 1]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+print(func(arr))
