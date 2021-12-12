d15fb42a712135303f025193346fd4d8c6b2c8ce
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-12-12 14:31:27 +0530

    Create firstIndex.cpp

---
+++ Recursions/firstIndex.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void displayArray(int arr[], int n){
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << ",";
+    }
+    
+}
+
+int firstIndex(int arr[], int n, int x, int si){
+    if (si == n){
+        return -1;
+    }
+
+    if (arr[si] == x){
+        return si;
+    }
+
+    int smallOutput = firstIndex(arr, n, x, si + 1);
+
+    if (smallOutput != -1){
+        return smallOutput;
+    }else{
+        return -1;
+    }
+}
+
+int main(){
+    int n, si, x;
+    cin >> n >> si >> x;
+
+    int arr[50];
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    displayArray(arr, n);
+
+    cout << firstIndex(arr, n, x,  si);
+
+    return 0;
+}
