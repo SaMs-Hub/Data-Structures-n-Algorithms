4eb84d23ff357436fe8fddd946096bd85daa22d2
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2021-12-20 22:25:49 +0530

    Update Delete.py

--- Arrays/Two Dimensional Arrays/Deleting an Element/Delete.py
+++ Arrays/Two Dimensional Arrays/Deleting an Element/Delete.py
@@ -1,6 +1,6 @@
 import numpy as np
 twoDArray = np.array([[11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9] ])
 
-#axis 0 represents rows, axis 1 represents columns
+# axis 0 represents rows, axis 1 represents columns
 newTDArray = np.delete(twoDArray, 1, axis=1)
 print(newTDArray)
