871e3dd1dce0d20d8da91b0d359bfa90bbd8e887
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-01-07 22:43:39 +0530

    Create numberToSpellings.cpp

---
+++ Recursions/numberToSpellings.cpp
@@ -0,0 +1,25 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+
+ char words[][10] = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+ void printSpellings(int n){
+     if (n == 0){
+         return;
+     }
+
+     printSpellings(n / 10);
+     int digit = n % 10;
+     cout << words[digit] << " ";
+     return;
+ }
+
+
+ int main(){
+     int n;
+     cin >> n;
+
+     printSpellings(n);
+
+     return 0;
+ }
