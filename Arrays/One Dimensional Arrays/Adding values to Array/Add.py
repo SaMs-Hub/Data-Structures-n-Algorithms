3ed2ba24518a4357db19ff03cc7dcde03d2bd069
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-01-08 17:59:37 +0530

    Update Add.py

--- Arrays/One Dimensional Arrays/Adding values to Array/Add.py
+++ Arrays/One Dimensional Arrays/Adding values to Array/Add.py
@@ -3,7 +3,7 @@ from array import *
 
 my_array = array('i',[1,2,3,4,5])
 
-#using append, element gets added at the last index
+# using append, element gets added at the last index
 my_array.append(6)
 print(my_array)
 
