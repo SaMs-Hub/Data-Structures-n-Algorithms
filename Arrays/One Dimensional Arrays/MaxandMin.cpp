e4781de107dd6541879a26756ecbd0a7a7013674
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-01-23 15:42:09 +0530

    Update MaxandMin.cpp

--- Arrays/One Dimensional Arrays/MaxandMin.cpp
+++ Arrays/One Dimensional Arrays/MaxandMin.cpp
@@ -1,4 +1,4 @@
-// Method 1
+// Method 1 Using language tools
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -79,4 +79,39 @@ int main(){
     func(arr, n);
     return 0;
 }
+
+
+// using standard way
+#include <bits/stdc++.h>
+using namespace std;
+
+int fuction(int arr[], int n){
+    int largest = arr[0];
+    int smallest = arr[0];
+
+    for (int i = 1; i <= n; i++){
+        if (arr[i] > largest){
+            largest = arr[i];
+        }
+        if (arr[i] < smallest){
+            smallest = arr[i];
+        }
+
+
+    }
+
+    cout << largest << smallest << endl;
+    return 0;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    int arr[] = {1, 2, 3, 4, 5};
+
+    cout << fuction(arr, n);
+
+}
+
 
