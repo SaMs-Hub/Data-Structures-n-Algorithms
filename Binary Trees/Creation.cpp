1ad0c9471bd4c55f2ebcb81897a09c595473396c
Shafi Khan <76864645+SaMs-Hub@users.noreply.github.com>
2022-04-12 21:07:15 +0530

    Create Creation.cpp

---
+++ Binary Trees/Creation.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+
+public:
+	int data;
+	Node *left;
+	Node *right;
+
+	Node(int data)
+	{
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+Node *buildTree()
+{
+	int d;
+	cin >> d;
+
+	if (d == -1)
+	{
+		return NULL;
+	}
+
+	Node *root = new Node(d);
+	root->left = buildTree();
+	root->right = buildTree();
+
+	return root;
+}
+
+void print(Node *root){
+	if (root == NULL){
+		return;
+	}
+
+	cout << root->data << " ";
+
+	print(root->left);
+	print(root->right);
+}
+
+int main()
+{
+
+	Node *root = buildTree();
+	print(root);
+	return 0;
+}
